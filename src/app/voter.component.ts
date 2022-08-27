f7fac57c3da1c98af5d1f176e2ce79c83838f933
trsnaidu143 <trsnaidu143@gmail.com>
2022-08-27 15:54:18 +0530

    Initial commit

---
+++ src/app/voter.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-profile-editor',
+  templateUrl: './profile-editor.component.html',
+  styleUrls: ['./profile-editor.component.css']
+})
+export class ProfileEditorComponent {
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
+}
