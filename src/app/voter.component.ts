454b5d7cd4789da03c92835bb3c1939875f1f59b
trsnaidu143 <trsnaidu143@gmail.com>
2022-08-27 16:03:09 +0530

    3 files modified, 2 added

--- src/app/voter.component.ts
+++ src/app/voter.component.ts
@@ -3,12 +3,16 @@ import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
-  templateUrl: './profile-editor.component.html',
-  styleUrls: ['./profile-editor.component.css']
+  templateUrl: './voter.component.html',
+  styleUrls: ['./voter.component.css'],
 })
 export class ProfileEditorComponent {
   profileForm = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
   });
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
 }
